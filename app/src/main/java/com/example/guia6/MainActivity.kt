da20262e89bffb85d258e27485c6535cd0188cf2
AlejandroFranco1101 <alejandrofranco1101@gmail.com>
2026-08-24 15:35:55 -0600

    pantalla principal y navegación
    pantalla principal y navegación

--- app/src/main/java/com/example/guia6/MainActivity.kt
+++ app/src/main/java/com/example/guia6/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.guia6
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun onClickFrame(view: View?) {
+        startActivity(Intent(this, FrameLayout::class.java))
+    }
+
+    fun onClickLinear(view: View?) {
+        startActivity(Intent(this, LinearLayout::class.java))
+    }
+
+    fun onClickRelative(view: View?) {
+        startActivity(Intent(this, RelativeLayout::class.java))
+    }
+
+    fun onClickTable(view: View?) {
+        startActivity(Intent(this, TableLayout::class.java))
+    }
+
+    fun onClickContraint(view: View?) {
+        startActivity(Intent(this, ConstraintLayout::class.java))
+    }
+
+    fun onClickGrid(view: View?) {
+        startActivity(Intent(this, GridLayout::class.java))
     }
 }
